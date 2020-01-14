15bb9fae6b8446e2a25280bf0e6f74c895974e17
Sam Langer <57724381+KazZBodnar@users.noreply.github.com>
2020-01-14 12:34:06 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import getpass
-from colorama import Fore
-print("Pypass")
+from colorama import Fore, Style
+print("2Password Password database")
 pin = getpass.getpass("Enter your pin: ")
 if pin == "PIN HERE":
   passw = getpass.getpass("Enter your password: ")
   if passw == "PASSWORD HERE":
-    print(Fore.GREEN + "Secret content")
+    print("")
+    print(Style.RESET_ALL + "Thing:", Fore.GREEN + "Password for the thing")
   else:
     print("Wrong password.")
 else:
